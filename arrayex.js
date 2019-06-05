116215b1f86a0eb9cd26a89f1a4c0aae1aed207a
Michael Garcia <33134352+mnitservices23@users.noreply.github.com>
2019-06-05 10:01:03 -0600

    Update arrayex.js

--- arrayex.js
+++ arrayex.js
@@ -1,1 +1,12 @@
-
+var array=[20,"yes",25];
+function arrayChecker(array1, index){
+  if (typeof array1[index]=="undefined"){
+      array[index]=0;
+  }else if(typeof array1[index]=="number"){
+     array[index]=1;
+  }else if(typeof array1[index]=="string"){
+     array[index]=2;
+  }
+  
+  return array1;
+}
